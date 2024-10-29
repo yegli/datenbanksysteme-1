e143e3ae5a203fead3b6e5b0bcf076abc9684915
Yanick Egli <nick.eglirohr@gmail.com>
2024-10-29 15:13:19 +0100

    updated schema based on testat 1 feedback

--- scripts-testat2/2_schema.sql
+++ scripts-testat2/2_schema.sql
@@ -4,48 +4,36 @@
  */
 
 CREATE TABLE person (
-  id INTEGER PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  vorname VARCHAR(255) NOT NULL,
   adresse VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   telefonnummer VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE mitarbeiter (
-  id INTEGER PRIMARY KEY REFERENCES person(id),
+  mitarbeiter_nummer INTEGER PRIMARY KEY REFERENCES person(id),
   abteilung VARCHAR(255) NOT NULL,
   anstellungsdatum DATE NOT NULL
 );
 
 CREATE TABLE verkäufer (id INTEGER PRIMARY KEY REFERENCES mitarbeiter(id));
 
-CREATE TABLE kunde (id INTEGER PRIMARY KEY REFERENCES person(id));
+CREATE TABLE kunde (kunden_nummer INTEGER PRIMARY KEY REFERENCES person(id));
 
 CREATE TABLE standort (
-  id INTEGER PRIMARY KEY,
   stadt VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE fahrzeug (
-  id INTEGER PRIMARY KEY,
+  fahrgestell_nummer INTEGER PRIMARY KEY,
   hersteller VARCHAR(255) NOT NULL,
   model VARCHAR(255) NOT NULL,
   baujahr INTEGER NOT NULL,
-  preis DECIMAL NOT NULL,
-  verfuegbarkeit BOOLEAN NOT NULL,
-  standort_id INTEGER REFERENCES standort(id)
+  preis DECIMAL NOT NULL
 );
 
 CREATE TABLE vertrag (
-  id INTEGER PRIMARY KEY,
   verkaufsdatum DATE NOT NULL,
-  zahlungsdatum DATE NOT NULL,
-  kunden_id INTEGER NOT NULL REFERENCES kunde(id),
-  fahrzeug_id INTEGER NOT NULL UNIQUE REFERENCES fahrzeug(id)  
-);
-
-CREATE TABLE fahrzeug_kunde (
-  fahrzeug_id INTEGER NOT NULL REFERENCES fahrzeug(id),
-  kunden_id INTEGER NOT NULL REFERENCES kunde(id),
-  PRIMARY KEY (fahrzeug_id, kunden_id)
+  zahlungsdatum DATE NOT NULL
 );
