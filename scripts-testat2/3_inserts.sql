d063505a149fb994bccd00ceb488cab7862d9f43
Yanick Egli <nick.eglirohr@gmail.com>
2024-10-29 22:32:19 +0100

    fixed inserts of testing data based on additional columns for fk

--- scripts-testat2/3_inserts.sql
+++ scripts-testat2/3_inserts.sql
@@ -7,26 +7,26 @@
 -- TABLE person
 
 INSERT INTO Person (id, name, vorname, adresse, email, telefonnummer, fk_kunde, fk_mitarbeiter) VALUES
-(1, 'John', 'Doe', '123 Main St', 'johndoe@example.com', '0798513647','NULL',10),
-(2, 'Jane', 'Smith', '456 Oak St', 'janesmith@example.com', '0776854235','NULL',9),
-(3, 'Michael', 'Brown', '789 Pine St', 'michaelbrown@example.com', '0776954832','NULL',8),
-(4, 'Emily', 'White', '135 Maple St', 'emilywhite@example.com', '0792345678','NULL',7),
-(5, 'David', 'Black', '246 Elm St', 'davidblack@example.com', '0779583434','NULL',6),
-(6, 'Sophia', 'Green', '357 Cedar St', 'sophiagreen@example.com', '0792349797','NULL',5),
-(7, 'James', 'Blue', '468 Birch St', 'jamesblue@example.com', '0771232992','NULL',4),
-(8, 'Emma', 'Red', '579 Willow St', 'emmared@example.com', '0796787878','NULL',3),
-(9, 'Lucas', 'Yellow', '681 Ash St', 'lucasyellow@example.com', '0771423647','NULL',2),
-(10, 'Olivia', 'Purple', '792 Spruce St', 'oliviapurple@example.com', '0791267352','NULL',1),
-(11, 'Liam', 'Orange', '893 Sycamore St', 'liamorange@example.com', '0771423564',2,'NULL'),
-(12, 'Ava', 'Pink', '904 Poplar St', 'avapink@example.com', '0796843674',3,'NULL'),
-(13, 'Noah', 'Brown', '015 Cedar St', 'noahbrown@example.com', '0779685869',4,'NULL'),
-(14, 'Isabella', 'White', '126 Birch St', 'isabellawhite@example.com', '0799654828',5,'NULL'),
-(15, 'Ethan', 'Black', '237 Willow St', 'ethenblack@example.com', '0772345342',6,'NULL'),
-(16, 'Sophia', 'Green', '348 Elm St', 'sophiagreen@example.com', '0776754976',7,'NULL'),
-(17, 'Mia', 'Blue', '459 Pine St', 'miablue@example.com', '0771423647',8,'NULL'),
-(18, 'Mason', 'Red', '560 Oak St', 'masonred@example.com', '0795674534',9,'NULL'),
-(19, 'Amelia', 'Yellow', '671 Main St', 'ameliayellow@example.com', '0798568546',10,'NULL'),
-(20, 'Elijah', 'Purple', '782 Spruce St', 'elijahpurple@example.com', '0771236363',1,'NULL');
+(1, 'John', 'Doe', '123 Main St', 'johndoe@example.com', '0798513647', NULL ,10),
+(2, 'Jane', 'Smith', '456 Oak St', 'janesmith@example.com', '0776854235', NULL ,9),
+(3, 'Michael', 'Brown', '789 Pine St', 'michaelbrown@example.com', '0776954832', NULL ,8),
+(4, 'Emily', 'White', '135 Maple St', 'emilywhite@example.com', '0792345678', NULL ,7),
+(5, 'David', 'Black', '246 Elm St', 'davidblack@example.com', '0779583434', NULL ,6),
+(6, 'Sophia', 'Green', '357 Cedar St', 'sophiagreen@example.com', '0792349797', NULL ,5),
+(7, 'James', 'Blue', '468 Birch St', 'jamesblue@example.com', '0771232992', NULL ,4),
+(8, 'Emma', 'Red', '579 Willow St', 'emmared@example.com', '0796787878', NULL ,3),
+(9, 'Lucas', 'Yellow', '681 Ash St', 'lucasyellow@example.com', '0771423647', NULL ,2),
+(10, 'Olivia', 'Purple', '792 Spruce St', 'oliviapurple@example.com', '0791267352', NULL ,1),
+(11, 'Liam', 'Orange', '893 Sycamore St', 'liamorange@example.com', '0771423564',2, NULL),
+(12, 'Ava', 'Pink', '904 Poplar St', 'avapink@example.com', '0796843674',3, NULL),
+(13, 'Noah', 'Brown', '015 Cedar St', 'noahbrown@example.com', '0779685869',4, NULL),
+(14, 'Isabella', 'White', '126 Birch St', 'isabellawhite@example.com', '0799654828',5, NULL),
+(15, 'Ethan', 'Black', '237 Willow St', 'ethenblack@example.com', '0772345342',6, NULL),
+(16, 'Sophia', 'Green', '348 Elm St', 'sophiagreen@example.com', '0776754976',7, NULL),
+(17, 'Mia', 'Blue', '459 Pine St', 'miablue@example.com', '0771423647',8, NULL),
+(18, 'Mason', 'Red', '560 Oak St', 'masonred@example.com', '0795674534',9, NULL),
+(19, 'Amelia', 'Yellow', '671 Main St', 'ameliayellow@example.com', '0798568546',10, NULL),
+(20, 'Elijah', 'Purple', '782 Spruce St', 'elijahpurple@example.com', '0771236363',1, NULL);
 
 -- TABLE mitarbeiter
 
@@ -86,28 +86,28 @@ INSERT INTO standort (id, stadt) VALUES
 
 -- TABLE fahrzeug
 
-INSERT INTO fahrzeug (fahrgestell_nummer, hersteller, model, baujahr, preis, standort) VALUES
-(1, 'Audi', 'A4', 2019, 40000),
-(2, 'BMW', 'X5', 2020, 50000),
-(3, 'Mercedes', 'C-Class', 2018, 45000),
-(4, 'Volkswagen', 'Golf', 2017, 30000),
-(5, 'Toyota', 'Corolla', 2016, 25000),
-(6, 'Ford', 'Focus', 2015, 20000),
-(7, 'Honda', 'Civic', 2014, 15000),
-(8, 'Hyundai', 'Elantra', 2013, 10000),
-(9, 'Kia', 'Rio', 2012, 8000),
-(10, 'Nissan', 'Sentra', 2011, 6000);
+INSERT INTO fahrzeug (fahrgestell_nummer, hersteller, model, baujahr, preis, fk_standort) VALUES
+(1, 'Audi', 'A4', 2019, 40000, 1),
+(2, 'BMW', 'X5', 2020, 50000, 2),
+(3, 'Mercedes', 'C-Class', 2018, 45000, 3),
+(4, 'Volkswagen', 'Golf', 2017, 30000, 4),
+(5, 'Toyota', 'Corolla', 2016, 25000, 5),
+(6, 'Ford', 'Focus', 2015, 20000, 6),
+(7, 'Honda', 'Civic', 2014, 15000, 7),
+(8, 'Hyundai', 'Elantra', 2013, 10000, 8),
+(9, 'Kia', 'Rio', 2012, 8000, 9),
+(10, 'Nissan', 'Sentra', 2011, 6000, 10);
 
 -- TABLE vertrag
 
-INSERT INTO vertrag (id, verkaufsdatum, zahlungsdatum) VALUES
-(1, '2022-03-01', '2022-03-01'),
-(2, '2022-03-02', '2022-03-02'),
-(3, '2022-03-03', '2022-03-03'),
-(4, '2022-03-04', '2022-03-04'),
-(5, '2022-03-05', '2022-03-05'),
-(6, '2022-03-06', '2022-03-06'),
-(7, '2022-03-07', '2022-03-07'),
-(8, '2022-03-08', '2022-03-08'),
-(9, '2022-03-09', '2022-03-09'),
-(10, '2022-03-10', '2022-03-10');
+INSERT INTO vertrag (id, verkaufsdatum, zahlungsdatum, fk_fahrzeug, fk_kunde) VALUES
+(1, '2022-03-01', '2022-03-01', 1, 10),
+(2, '2022-03-02', '2022-03-02', 2, 9),
+(3, '2022-03-03', '2022-03-03', 3, 8),
+(4, '2022-03-04', '2022-03-04', 4, 7),
+(5, '2022-03-05', '2022-03-05', 5, 6),
+(6, '2022-03-06', '2022-03-06', 6, 5),
+(7, '2022-03-07', '2022-03-07', 7, 4),
+(8, '2022-03-08', '2022-03-08', 8, 3),
+(9, '2022-03-09', '2022-03-09', 9, 2),
+(10, '2022-03-10', '2022-03-10', 10, 1);
